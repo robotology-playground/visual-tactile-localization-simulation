1dd6d57e1788f3ab4a75f873b97fe9c287e2530e
Nicola Piga <nicolapiga@gmail.com>
2018-05-16 18:31:39 +0200

    add support to idl thrift

---
+++ idl.thrift
@@ -0,0 +1,12 @@
+service VIS_TAC_IDL
+{
+    string localize();
+    string move_hand_upward(1:string armToMove);
+    string home(1:string armToPutHome);
+    string approach(1:string armToUse, 2:string whereToApproach);
+    string fingers_approach(1:string armToUse);
+    string pull(1:string armToUse);
+    string rotate(1:string armToUse);
+    string stop();
+    string quit();
+}
