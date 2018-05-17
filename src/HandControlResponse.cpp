0d228dec8633ed02b4c9c7b661ae93fa584f80c5
Nicola Piga <nicolapiga@gmail.com>
2018-05-17 15:12:28 +0200

    untabify src/HandControlResponse.cpp

--- src/HandControlResponse.cpp
+++ src/HandControlResponse.cpp
@@ -12,8 +12,8 @@
 #include "HandControlResponse.h"
 
 HandControlResponse::HandControlResponse() : is_approach_done(false),
-					     is_restore_done(false),
-					     response(Response::Empty) {};
+                                             is_restore_done(false),
+                                             response(Response::Empty) {};
 
 void HandControlResponse::setIsApproachDone(const bool &is_done)
 {
@@ -23,15 +23,15 @@ void HandControlResponse::setIsApproachDone(const bool &is_done)
 
 void HandControlResponse::setIsRestoreDone(const bool &is_done)
 {
-    response = Response::RestoreStatus;    
+    response = Response::RestoreStatus;
     is_restore_done = is_done;
 }
 
 bool HandControlResponse::isApproachDone(bool &is_done) const
 {
     if (response != Response::ApproachStatus)
-	return false;
-    
+        return false;
+
     is_done = is_approach_done;
 
     return true;
@@ -40,10 +40,10 @@ bool HandControlResponse::isApproachDone(bool &is_done) const
 bool HandControlResponse::isRestoreDone(bool &is_done) const
 {
     if (response != Response::RestoreStatus)
-	return false;
+        return false;
 
     is_done = is_restore_done;
-    
+
     return true;
 }
 
@@ -57,15 +57,15 @@ void HandControlResponse::clear()
 bool HandControlResponse::read(yarp::os::ConnectionReader& connection)
 {
     clear();
-    
+
     response = static_cast<Response>(connection.expectInt());
 
     if (response != Response::Empty)
     {
-	if (response == Response::ApproachStatus)
-	    is_approach_done = connection.expectInt();
-	else if (response == Response::RestoreStatus)
-	    is_restore_done = connection.expectInt();
+        if (response == Response::ApproachStatus)
+            is_approach_done = connection.expectInt();
+        else if (response == Response::RestoreStatus)
+            is_restore_done = connection.expectInt();
     }
 
     return !connection.isError();
@@ -77,10 +77,10 @@ bool HandControlResponse::write(yarp::os::ConnectionWriter& connection)
 
     if (response != Response::Empty)
     {
-	if (response == Response::ApproachStatus)
-	    connection.appendInt(is_approach_done);
-	else if (response == Response::RestoreStatus)
-	    connection.appendInt(is_restore_done);
+        if (response == Response::ApproachStatus)
+            connection.appendInt(is_approach_done);
+        else if (response == Response::RestoreStatus)
+            connection.appendInt(is_restore_done);
     }
 
     return !connection.isError();
