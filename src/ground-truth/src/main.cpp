a23ec7c1481a2c3fbc48a69c9933b2502898ed8a
Nicola Piga <nicolapiga@gmail.com>
2018-06-14 18:09:15 +0200

    instantiate class Tracker in main.cpp

--- src/ground-truth/src/main.cpp
+++ src/ground-truth/src/main.cpp
@@ -29,6 +29,6 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    // Tracker tracker;
-    // return tracker.runModule(rf);
+    Tracker tracker;
+    return tracker.runModule(rf);
 }
