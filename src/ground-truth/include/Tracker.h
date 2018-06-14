90867e25d6ba80e88e4edc087c95c5a337d26bba
Nicola Piga <nicolapiga@gmail.com>
2018-06-14 18:15:06 +0200

    layout of ground truth tracker ready

--- src/ground-truth/include/Tracker.h
+++ src/ground-truth/include/Tracker.h
@@ -8,3 +8,52 @@
 /**
  * @authors: Nicola Piga
  */
+
+#ifndef TRACKER_H
+#define TRACKER_H
+
+// yarp
+#include <yarp/os/RFModule.h>
+#include <yarp/sig/Image.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/IFrameTransform.h>
+
+#include <HeadKinematics.h>
+
+class Tracker : public yarp::os::RFModule
+{
+private:
+    // head kinematics
+    headKinematics head_kin;
+
+    // camera port
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> image_input_port;
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> image_output_port;
+
+    // selected eye name
+    std::string eye_name;
+
+    // period
+    double period;
+
+    // frame transform client
+    yarp::dev::PolyDriver drv_transform_client;
+    yarp::dev::IFrameTransform* tf_client;
+    std::string tf_source;
+    std::string tf_target;
+
+    // camera parameters
+    double cam_fx;
+    double cam_fy;
+    double cam_cx;
+    double cam_cy;
+
+public:
+    bool configure(yarp::os::ResourceFinder &rf) override;
+    double getPeriod() override;
+    bool getFrame(yarp::sig::ImageOf<yarp::sig::PixelRgb>* &yarp_image);
+    bool updateModule() override;
+    bool close() override;
+};
+
+#endif
--- src/ground-truth/src/Tracker.cpp
+++ src/ground-truth/src/Tracker.cpp
@@ -10,184 +10,147 @@
  */
 
 // yarp
-#include <yarp/os/Network.h>
 #include <yarp/os/ResourceFinder.h>
 #include <yarp/os/LogStream.h>
-#include <yarp/os/RFModule.h>
-#include <yarp/sig/Image.h>
-#include <yarp/dev/PolyDriver.h>
-#include <yarp/dev/IFrameTransform.h>
 
 // opencv
 #include <opencv2/opencv.hpp>
 
 //
-#include <HeadKinematics.h>
-#include <cstdlib>
+#include <Tracker.h>
 
-class Tracker : public yarp::os::RFModule
+bool Tracker::configure(yarp::os::ResourceFinder &rf)
 {
-private:
-    // head kinematics
-    headKinematics head_kin;
-    
-    // camera port
-    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> image_input_port;
-    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> image_output_port;
+    // get parameter from the configuration
+    std::string robot_name = "icub";
+    if (!rf.find("robotName").isNull())
+        robot_name = rf.find("robotName").asString();
 
-    // selected eye name
-    std::string eye_name;
+    eye_name = "left";
+    if (!rf.find("eyeName").isNull())
+        eye_name = rf.find("eyeName").asString();
 
-    // period
-    double period;
+    double cam_width = 320;
+    if (!rf.find("camWidth").isNull())
+        cam_width = rf.find("camWidth").asDouble();
+
+    double cam_height = 240;
+    if (!rf.find("camHeight").isNull())
+        cam_height = rf.find("camHeight").asDouble();
+
+    period = 0.03;
+    if (!rf.find("period").isNull())
+        period = rf.find("period").asDouble();
+
+    tf_source = "/iCub/frame";
+    if (!rf.find("tfSource").isNull())
+        tf_source = rf.find("tfSource").asString();
+    tf_target = "/ground_truth/frame";
+    if (!rf.find("tfTarget").isNull())
+        tf_target = rf.find("tfTarget").asString();
+
+    // set intrinsic parameters
+    const yarp::os::ResourceFinder &rf_eye = rf.findNestedResourceFinder(eye_name.c_str());
+    cam_fx = 219.057;
+    cam_cx = 174.742;
+    cam_fy = 219.028;
+    cam_cy = 102.874;
+    if (!rf_eye.find("camFx").isNull())
+        cam_fx = rf_eye.find("camFx").asDouble();
+    if (!rf_eye.find("camCx").isNull())
+        cam_cx = rf_eye.find("camCx").asDouble();
+    if (!rf_eye.find("camFy").asDouble())
+        cam_fy = rf_eye.find("camFy").asDouble();
+    if (!rf_eye.find("camCy").asDouble())
+        cam_cy = rf_eye.find("camCy").asDouble();
+
+    // port prefix
+    std::string port_prefix = "/gtruth_tracker/";
+    port_prefix += eye_name + "_eye";
+
+    // open port
+    bool port_ok = image_input_port.open(port_prefix + ":i");
+    if (!port_ok)
+        return false;
+    port_ok = image_output_port.open(port_prefix + ":o");
+    if (!port_ok)
+        return false;
+
+    // head forward kinematics
+    if (!head_kin.configure(robot_name, port_prefix))
+        return false;
 
     // frame transform client
-    yarp::dev::PolyDriver drv_transform_client;    
-    yarp::dev::IFrameTransform* tf_client;
-    std::string tf_source;
-    std::string tf_target;
+    yarp::os::Property propTfClient;
+    propTfClient.put("device", "FrameTransformClient");
+    propTfClient.put("local", port_prefix + "/transformClient");
+    propTfClient.put("remote", "/transformServer");
+    tf_client = nullptr;
+    bool ok_drv = drv_transform_client.open(propTfClient);
+    ok_drv = ok_drv && drv_transform_client.view(tf_client) && tf_client != nullptr;
+    if (!ok_drv)
+        return false;
 
-    // camera parameters
-    double cam_fx;
-    double cam_fy;
-    double cam_cx;
-    double cam_cy;
+    return true;
+}
 
-public:
-    bool configure(yarp::os::ResourceFinder &rf) override
-    {
-        // get parameter from the configuration
-        std::string robot_name = "icub";
-        if (!rf.find("robotName").isNull())
-            robot_name = rf.find("robotName").asString();
-        
-        eye_name = "left";
-        if (!rf.find("eyeName").isNull())
-            eye_name = rf.find("eyeName").asString();
+double Tracker::getPeriod()
+{
+    return period;
+}
 
-        double cam_width = 320;
-        if (!rf.find("camWidth").isNull())
-            cam_width = rf.find("camWidth").asDouble();
+bool Tracker::getFrame(yarp::sig::ImageOf<yarp::sig::PixelRgb>* &yarp_image)
+{
+    // try to read image from port
+    yarp_image = image_input_port.read(false);
 
-        double cam_height = 240;
-        if (!rf.find("camHeight").isNull())
-            cam_height = rf.find("camHeight").asDouble();
+    if (yarp_image == NULL)
+        return false;
 
-        period = 0.03;
-        if (!rf.find("period").isNull())
-            period = rf.find("period").asDouble();
+    return true;
+}
 
-	tf_source = "/iCub/frame";
-        if (!rf.find("tfSource").isNull())
-            tf_source = rf.find("tfSource").asString();
-        tf_target = "/ground_truth/frame";
-        if (!rf.find("tfTarget").isNull())
-            tf_target = rf.find("tfTarget").asString();
-
-        // port prefix
-        std::string port_prefix = "/gtruth_tracker/";
-        port_prefix += eye_name + "_eye";
-	
-        // open port
-        bool port_ok = image_input_port.open(port_prefix + ":i");
-        if (!port_ok)
-            return false;
-        port_ok = image_output_port.open(port_prefix + ":o");
-        if (!port_ok)
-            return false;
-
-        // head forward kinematics
-        if (!head_kin.configure(robot_name, port_prefix))
-            return false;
-
-        // frame transform client
-	yarp::os::Property propTfClient;
-	propTfClient.put("device", "FrameTransformClient");
-	propTfClient.put("local", port_prefix + "/transformClient");
-	propTfClient.put("remote", "/transformServer");
-	tf_client = nullptr;
-	bool ok_drv = drv_transform_client.open(propTfClient);
-	ok_drv = ok_drv && drv_transform_client.view(tf_client) && tf_client != nullptr;
-	if (!ok_drv)
-	    return false;
-
-        // set intrinsic parameters
-        double cam_fx;
-        double cam_fy;
-        double cam_cx;
-        double cam_cy;
-        if (eye_name == "left")
-        {
-            cam_fx = 219.057;
-            cam_cx = 174.742;
-            cam_fy = 219.028;
-            cam_cy = 102.874;
-        }
-        else if (eye_name == "right")
-        {
-            cam_fx = 225.904;
-            cam_cx = 157.858;
-            cam_fy = 227.041;
-            cam_cy = 113.51;
-        }
-
+bool Tracker::updateModule()
+{
+    // get image from camera
+    yarp::sig::ImageOf<yarp::sig::PixelRgb>* img_in;
+    if (!getFrame(img_in))
         return true;
-    }
 
-    double getPeriod() override
-    {
-        return period;
-    }
+    // get current pose of eyes
+    yarp::sig::Vector left_eye_pose;
+    yarp::sig::Vector right_eye_pose;
+    yarp::sig::Vector eye_pose;
+    head_kin.getEyesPose(left_eye_pose, right_eye_pose);
+    eye_pose = (eye_name == "left") ? left_eye_pose : right_eye_pose;
 
-    bool getFrame(yarp::sig::ImageOf<yarp::sig::PixelRgb>* &yarp_image)
-    {
-	// try to read image from port
-	yarp_image = image_input_port.read(false);
+    // TODO: add aruco estimation here
 
-	if (yarp_image == NULL)
-	    return false;
+    // TODO: add gaze tracking here
 
-	return true;
-    }
+    // prepare output image
+    // yarp::sig::ImageOf<yarp::sig::PixelRgb> &img_out = image_output_port.prepare();
+    // img_out = *img_in;
+    // cv::Mat cv_img;
+    // cv_img = cv::cvarrToMat(img_out.getIplImage());
 
-    bool updateModule() override
-    {
-	// get image from camera
-	yarp::sig::ImageOf<yarp::sig::PixelRgb>* img_in;
-	if (!getFrame(img_in))
-	    return true;
+    // send image
+    image_output_port.write();
 
-        // get current pose of eyes
-        yarp::sig::Vector left_eye_pose;
-        yarp::sig::Vector right_eye_pose;
-        yarp::sig::Vector eye_pose;
-        head_kin.getEyesPose(left_eye_pose, right_eye_pose);
-        eye_pose = (eye_name == "left") ? left_eye_pose : right_eye_pose;
+    return true;
+}
 
-        // prepare output image
-        // yarp::sig::ImageOf<yarp::sig::PixelRgb> &img_out = image_output_port.prepare();
-        // img_out = *img_in;
-        // cv::Mat cv_img;
-        // cv_img = cv::cvarrToMat(img_out.getIplImage());
+bool Tracker::close()
+{
+    // close ports
+    image_input_port.close();
+    image_output_port.close();
 
-        // send image
-        image_output_port.write();
+    // close head kinematics
+    head_kin.close();
 
-        return true;
-    }
+    // close transform client
+    drv_transform_client.close();
 
-    bool close() override
-    {
-	// close ports
-	image_input_port.close();
-        image_output_port.close();        
-        
-	// close head kinematics
-	head_kin.close();
-
-        // close transform client
-        drv_transform_client.close();        
-
-        return true;
-    }
-};
+    return true;
+}
